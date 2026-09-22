eaca3fdc72b730b5c7fe759eb021c70bb5918759
MAKSOOD402 <maksoodbasha402@gmail.com>
2026-09-22 15:56:02 +0530

    connected to Server ASPDOTNET URL

--- student-marks-react/src/App.jsx
+++ student-marks-react/src/App.jsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useState } from 'react';
 import './App.css';
 
-const API_URL = 'http://localhost:5150/api/StudentMark';
+const API_URL = 'http://studentmarks-api-dgfrfderbmcpbegk.southindia-01.azurewebsites.net/api/StudentMark';
 
 export default function App() {
   const [students, setStudents] = useState([]);
