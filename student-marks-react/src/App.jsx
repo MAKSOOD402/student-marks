b12d186235fc0631a051d3701d5afaff3f9b73f8
MAKSOOD402 <maksoodbasha402@gmail.com>
2026-09-23 13:17:26 +0530

    react now calling proper ASP.NET

--- student-marks-react/src/App.jsx
+++ student-marks-react/src/App.jsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useState } from 'react';
 import './App.css';
 
-const API_URL = 'http://studentmarks-api-dgfrfderbmcpbegk.southindia-01.azurewebsites.net/api/StudentMark';
+const API_URL = 'https://studentmarks-api-dgfrfderbmcpbegk.southindia-01.azurewebsites.net/api/StudentMark';
 
 export default function App() {
   const [students, setStudents] = useState([]);
